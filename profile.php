990004752d2674335bc58f063f05833e41e39cb9
Durgesh Agrawal <102041586+Dmaster007@users.noreply.github.com>
2022-09-27 21:36:23 +0530

    updated navbar

--- profile.php
+++ profile.php
@@ -19,13 +19,64 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   <!-- <link rel="stylesheet" href="files/css/bootstrap.css"> -->
   <link rel="stylesheet" href="files/css/custom.css">
+  <style>
+    
+.navbar-custom {
+	background-color: rgb(28, 169, 122);
+}
+
+  </style>
 </head>
 
 <body>
 
+  <nav class="navbar navbar-dark navbar-custom navbar-light  fixed-top bg-none" >
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Hostel Portal</a>
+      <button class="navbar-toggler shadow-none" style="border-width:0 ;" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel"></h5>
+          <button type="button" class="btn-close btn-close-white shadow-none"  style="border-width:0px ;" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="profile.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link"   style="color: white;" href="message.php">Add Complaints</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link"  style="color: white;" href="status.php">View Status</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link"  style="color: white;" href="logout.php" onClick="javascript:return confirm ('Do you really want to Logout ?');">Logout</a>
+            </li>
+            <!-- <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li> -->
+          </ul>
+         
+        </div>
+      </div>
+    </div>
+  </nav>
   <div class="coverusr" style="height: 300px; ;">
 
-    <nav class="nav_u">
+    <!-- <nav class="nav_u">
 
       
       <ul>
@@ -40,10 +91,10 @@
 
 
 
-    </nav>
+    </nav> -->
 
     
-    <p class="text-right pdd" style="padding-left:85%">
+    <p class="text-right pdd" style="padding-left:75%">
       <?php echo date("d M , l "); ?>
     </p>
   </div>
