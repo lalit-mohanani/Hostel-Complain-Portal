e889af8bf75cdf8165cd5fce36bad276631e1bb4
Lalit Mohanani <85385420+lalit-mohanani@users.noreply.github.com>
2022-10-15 12:52:11 +0530

    Update general.php

--- admin/general.php
+++ admin/general.php
@@ -4,10 +4,10 @@ require '../core/session.php';
 // require '../core/config.php';
 require '../core/admin-key.php';
 
-$host = "localhost";
-$database = "hrmd";
-$username = "root";
-$password = "";
+$host = "db4free.net"; 
+$database = "hostel_db";
+$username = "webnduser";
+$password = "webndPass";
 
 
 $conn = mysqli_connect($host, $username, $password, $database);
